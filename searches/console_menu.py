cdc483d9e20ef775c778ad16fab2df5818b12bb7
noam <noam88888@gmail.com>
2022-02-10 23:28:36 +0200

    made the second search accessible

--- searches/console_menu.py
+++ searches/console_menu.py
@@ -3,6 +3,7 @@ import sys
 import ma02spotipy.constants as const
 from ma02spotipy.extract.parser_songs import *
 from ma02spotipy.load.load_to_objects import *
+from ma02spotipy.searches.searches_on_data import *
 from ma02spotipy.user.user import *
 
 
@@ -67,6 +68,13 @@ def menu():
         set_artists, aet_albums = load_to_objects(list_of_tracks(const.SONGS_DIRECTORY_FOR_MENU))
         for artist in set_artists:
             print(artist.__str__())
+            print("")
+        menu()
+    elif choice == "B" or choice == "b":
+        artist_id = input("Put the artist id: ")
+        albums = get_albums_by_artist_id(artist_id)
+        for album in albums:
+            print(album.__str__())
             print("")
         menu()
 
