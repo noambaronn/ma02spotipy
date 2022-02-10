96f52a978385764a459ae3e1e69108e3ffce8780
noam <noam88888@gmail.com>
2022-02-10 23:17:19 +0200

    third search : best songs of artist

--- searches/searches_on_data.py
+++ searches/searches_on_data.py
@@ -23,3 +23,27 @@ def get_songs_in_album(album_id: str):
             return album.songs
     if is_exist == False:
         raise AlbumNotExist
+
+
+def get_all_the_artists_songs(artist_id: str):
+    is_artist_exist = False
+    all_the_songs_of_artist = []
+    set_of_artists, set_of_albums = load_to_objects(list_of_tracks())
+    for artist in set_of_artists:
+        if artist.id_of_artist == artist_id:
+            is_artist_exist = True
+            for album in artist.albums_of_artist:
+                for song in album.songs:
+                    all_the_songs_of_artist.append(song)
+                return all_the_songs_of_artist, is_artist_exist
+
+
+def get_the_best_songs_by_popularity(artist_id: str, is_premium: bool):
+    artists_songs, is_artist_exist = get_all_the_artists_songs(artist_id)
+    if is_artist_exist == False:
+        raise ArtistNotExist
+    artists_songs = sorted(artists_songs, key=lambda song: song.popularity, reverse=True)
+    if is_premium:
+        return artists_songs[:10]
+    else:
+        return artists_songs[:5]
