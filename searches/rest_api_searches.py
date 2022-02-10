0e59d42f8f9e4a73f1693cc3a47dd2619f9cd083
noam <noam88888@gmail.com>
2022-02-10 23:34:27 +0200

    tried to  button  the first two searches for the user
    rest api

---
+++ searches/rest_api_searches.py
@@ -0,0 +1,25 @@
+import flask
+from ma02spotipy.searches.searches_on_data import *
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/getAllArtists/', methods=['GET'])
+def get_all_artists():
+    set_artists, set_albums = load_to_objects(list_of_tracks(const.SONGS_DIRECTORY_FOR_MENU))
+    artists = {}
+    artists["artists"] = set_artists
+    return artists
+
+
+@app.route('/getAllAlbumsOf/artist_id/', methods=['GET'])
+def get_albums_by_id():
+    albums = get_albums_by_artist_id('5EgEUxXi9uTYYN4cQwmLPy')
+    albums_of_artist = {}
+    albums_of_artist["albums"] = albums
+    return albums_of_artist
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, use_reloader=False)
