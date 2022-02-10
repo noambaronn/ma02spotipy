60117d0707c7bd6747e81a3df90b6b88d24d1dc3
noam <noam88888@gmail.com>
2022-02-10 23:16:09 +0200

    first&second searches : get albums of artist by id and get songs of album by id
    did not mean to put the first search staged change in branch user

--- searches/searches_on_data.py
+++ searches/searches_on_data.py
@@ -1,5 +1,6 @@
 from ma02spotipy.extract.parser_songs import *
 from ma02spotipy.load.load_to_objects import *
+from ma02spotipy.exceptions.exceptions import *
 import ma02spotipy.constants as const
 
 
@@ -11,3 +12,14 @@ def get_albums_by_artist_id(artist_id: str):
             for album in artist.albums_of_artist:
                 albums += album.name_of_album
             return albums
+
+
+def get_songs_in_album(album_id: str):
+    is_exist = False
+    set_of_artists, set_of_albums = load_to_objects(list_of_tracks())
+    for album in set_of_albums:
+        if album.id_of_album == album_id:
+            is_exist = True
+            return album.songs
+    if is_exist == False:
+        raise AlbumNotExist
